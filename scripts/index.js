9634c2a0297d993dcf75f76a120282e88194e5ca
anacarladsa <anacarla1724@gmail.com>
2022-09-14 19:32:51 -0300

    aula1409

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+function alert1(){
+    alert("Show!");
+}
+function receitaBolo(){
+    let nomeDoBolo = "Bolo de manteiga"
+    let farinhaTrigo = 200;
+    let ovos = 4;
+    let manteiga = 100.85;
+    console.log(nomeDoBolo,typeof nomeDoBolo);
+    console.log(farinhaTrigo,typeof farinhaTrigo);
+    console.log(ovos,typeof ovos);
+    console.log(manteiga,typeof manteiga);
+    manteiga = "italac"
+    console.log(manteiga,typeof manteiga);
+}
